e4c4f794fe730f93d20a26c925287113085c395c
Manjunath <Manjunath@TYSS-ELF-Manjunath>
2020-01-31 17:22:46 +0530

    5* by user 1

--- InternetExplorer/src/iE/InternetExplorer.java
+++ InternetExplorer/src/iE/InternetExplorer.java
@@ -24,7 +24,7 @@ public class InternetExplorer {
 		System.out.println("Already Pulled by user 1");
 
 		System.out.println("Already Pulled by user 2");
-		System.out.println("*****");
+		System.out.println("***** by user 1");
 
 	}
 
