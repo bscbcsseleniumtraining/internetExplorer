dda60cc3d286c43de4c04f603a4cd3bce5d03712
Manjunath <Manjunath@TYSS-ELF-Manjunath>
2020-01-31 16:58:21 +0530

    Change 2 by User 1

--- InternetExplorer/src/iE/InternetExplorer.java
+++ InternetExplorer/src/iE/InternetExplorer.java
@@ -19,6 +19,7 @@ public class InternetExplorer {
 		int p = driver.manage().window().getPosition().getX();
 		System.out.println(p);
 		System.out.println("Change 1 by User 1");
+		System.out.println("Change 2 by User 1");
 	}
 
 }
