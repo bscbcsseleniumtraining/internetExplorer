7afbc696cb7af4db2adb840de7514c44794eb5ff
Manjunath <Manjunath@TYSS-ELF-Manjunath>
2020-01-31 16:18:31 +0530

    My 1st Commit

---
+++ InternetExplorer/src/iE/InternetExplorer.java
@@ -0,0 +1,23 @@
+package iE;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class InternetExplorer {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = new InternetExplorerDriver();
+		driver.manage().window().maximize();
+//		driver.get("http://localhost:8888");
+//		
+//		driver.findElement(By.name("user_name")).sendKeys("admin");
+//		driver.findElement(By.name("user_password")).sendKeys("manager",Keys.ENTER);
+		int p = driver.manage().window().getPosition().getX();
+		System.out.println(p);
+	}
+
+}
